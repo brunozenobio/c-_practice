1c111b1522746b558a9d97f951d30c5f3a3593cf
brunozenobio <brunozenobio4@gmail.com>
2025-01-27 19:16:41 -0300

    read csv

---
+++ src/data_struct/table.h
@@ -0,0 +1,17 @@
+#ifndef LINKEDLIST
+#define LINKEDLIST
+#include <iostream>
+#include <string>
+using namespace std;
+
+class CSVToTable{
+    private:
+        int num_features;
+        int num_rows;
+        
+        
+
+
+
+
+};

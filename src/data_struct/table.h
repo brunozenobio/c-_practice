96aaed00968a6f986863ba708e2429092e65a3f0
bruno <bruno@brunopc.brunopc>
2025-01-27 23:31:10 -0300

    Method readCSv

--- src/data_struct/table.h
+++ src/data_struct/table.h
@@ -2,16 +2,53 @@
 #define LINKEDLIST
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <variant>
 using namespace std;
-
+using DataType = variant<int,double,string>;
 class CSVToTable{
     private:
         int num_features;
         int num_rows;
+        vector<vector<DataType>> data;
+    public:
+        CSVToTable(){} 
         
+        void readCSV(const string& filename,const string& sep){
+            ifstream file(filename);
+            string line;
+            // Ver como poner estoen metodos
+            while (getline(file,line)){
+                stringstream ss(line);
+                string value;
+                int col_index = 0;
+                while (getline(ss,value,sep)){
+                    if (value.find('.')!=string::npos){
+                        try{
+                            data[col_index].push_back(stod(value));
+                        } catch(...){
+                            data[col_index].push_back(value);
+                        }
+                        
+                    }else{
+                        try{
+                            data[col_index].push_back(stoi(value));
+                        }catch(...){
+                            data[col_index].push_back(value);
+                        }
+                    }
+                    col_index++;
+                }
+                
+            }
+
+        }
         
 
 
 
 
 };
+#endif
